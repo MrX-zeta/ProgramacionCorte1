f8053ad7fafea15698522398b5462b76998b6629
MrX-zeta <brianluisruizperez@gmail.com>
2025-02-02 14:55:22 -0600

    Primer commit - Subida del código

---
+++ EdadesDoWhile.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int menores24, mayores46, n, edad, entre25y45, contador;
+    menores24 = 0;
+    mayores46 = 0;
+    entre25y45 = 0;
+    contador = 0;
+    cout<<"Ingrese la cantidad de personas: "; cin>>n;
+    do{
+        cout<<"Ingrese la edad: "; cin>>edad;
+        if(edad < 24){
+            menores24 ++;
+        }
+        else if(edad > 46){
+            mayores46 ++;
+        }
+        else if(edad >= 25 and edad <= 45){
+            entre25y45 ++;
+        }
+        contador ++;
+    }while(contador < n);
+    cout<<"Menores que 24: "<<menores24<<endl;
+    cout<<"Mayores que 46: "<<mayores46<<endl;
+    cout<<"Rango entre 25 y 45: "<<entre25y45<<endl;
+    return 0;
+}
--- proyectoCorte1.cpp
+++ proyectoCorte1.cpp
@@ -12,23 +12,26 @@ int main(){
     while(b<=0){
         cout<<"El valor de b debe ser mayor a 0. Ingrese nuevamente el valor de b: "; cin>>b;
     }
-    c = sqrt(pow(a,2) + pow(b,2));
+    c = sqrt(pow(a, 2) + pow(b, 2));
+    c = round(c * 100) / 100;
     cout<<"El valor de la hipotenusa es: "<<c<<endl;
     perimetro = a+b+c;
     cout<<"El perimetro del triangulo es: "<<perimetro<<endl;
     angulo = atan(a/b) * (180.0 / M_PI); //Conversion de radianes a grados
     angulo = round(angulo * 100) / 100;
     cout<<"El angulo del triangulo en grados es: "<<angulo<<" grados"<<endl;
-    if(a==b){
+    if(a == b){
         cout<<"El triangulo es isosceles"<<endl;
     }
     else{
         cout<<"El triangulo no es isosceles"<<endl;
     }
-    if (pow(a, 2) + pow(b, 2) == pow(c, 2)) {
-        cout << "Este es un triángulo rectangulo." << endl;
-    } else {
-        cout << "Este NO es un triángulo rectangulo." << endl;
+    
+    if (a * a + b * b == c * c){
+        cout << "Este es un triangulo rectangulo."<<endl;
+    } 
+    else {
+        cout << "Este NO es un triangulo rectangulo."<<endl;
     }
     return 0;
 }
